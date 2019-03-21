14ecb819957ae440f54870ef996835faeae97ade
Alexander <vorobyovvorobyov@gmail.com>
2019-03-21 21:48:43 +0300

    Add UGuid

---
+++ Packages/UGF.Math/Editor/UGuidDrawer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UGF.Math.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.Math.Editor
+{
+    [CustomPropertyDrawer(typeof(UGuid))]
+    internal sealed class UGuidDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty propertyValue0 = property.FindPropertyRelative("m_value0");
+            SerializedProperty propertyValue1 = property.FindPropertyRelative("m_value1");
+
+            var uguid = new UGuid(propertyValue0.longValue, propertyValue1.longValue);
+            string value = uguid.ToString();
+
+            string changed = EditorGUI.DelayedTextField(position, label, value);
+            
+            if (value != changed)
+            {
+                value = changed;
+                
+                if (Guid.TryParse(value, out Guid guid))
+                {
+                    uguid = guid;
+
+                    propertyValue0.longValue = uguid.Value0;
+                    propertyValue1.longValue = uguid.Value1;
+                }
+                else
+                {
+                    Debug.LogWarning($"Invalid value for guid: '{value}'.");
+                }
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+    }
+}
---
+++ Packages/UGF.Math/Runtime/UGuid.cs
@@ -0,0 +1,147 @@
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace UGF.Math.Runtime
+{
+    [Serializable]
+    public struct UGuid
+    {
+        [SerializeField] private long m_value0;
+        [SerializeField] private long m_value1;
+
+        public long Value0 { get { return m_value0; } }
+        public long Value1 { get { return m_value1; } }
+
+        public static UGuid Empty { get { return m_empty; } }
+
+        private static readonly Converter m_converter = new Converter();
+        private static readonly UGuid m_empty;
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct Converter
+        {
+            [FieldOffset(0)] public Guid Guid;
+            [FieldOffset(0)] public UGuid UGuid;
+        }
+
+        static UGuid()
+        {
+            Converter converter = m_converter;
+
+            converter.Guid = Guid.Empty;
+
+            m_empty = converter.UGuid;
+        }
+
+        public UGuid(long value0, long value1)
+        {
+            m_value0 = value0;
+            m_value1 = value1;
+        }
+
+        public UGuid(UGuid uguid)
+        {
+            m_value0 = uguid.Value0;
+            m_value1 = uguid.Value1;
+        }
+
+        public UGuid(Guid guid)
+        {
+            Converter converter = m_converter;
+
+            converter.Guid = guid;
+
+            UGuid uguid = converter.UGuid;
+
+            m_value0 = uguid.Value0;
+            m_value1 = uguid.Value1;
+        }
+
+        public UGuid(string guid)
+        {
+            Converter converter = m_converter;
+
+            converter.Guid = new Guid(guid);
+
+            UGuid uguid = converter.UGuid;
+
+            m_value0 = uguid.Value0;
+            m_value1 = uguid.Value1;
+        }
+
+        public byte[] ToByteArray()
+        {
+            Converter converter = m_converter;
+
+            converter.UGuid = this;
+
+            return converter.Guid.ToByteArray();
+        }
+
+        public bool Equals(UGuid other)
+        {
+            return Value0 == other.Value0 && Value1 == other.Value1;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is UGuid other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Value0.GetHashCode() * 397) ^ Value1.GetHashCode();
+            }
+        }
+
+        public static bool operator ==(UGuid left, UGuid right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(UGuid left, UGuid right)
+        {
+            return !left.Equals(right);
+        }
+
+        public static implicit operator Guid(UGuid uguid)
+        {
+            Converter converter = m_converter;
+
+            converter.UGuid = uguid;
+
+            return converter.Guid;
+        }
+
+        public static implicit operator UGuid(Guid guid)
+        {
+            Converter converter = m_converter;
+
+            converter.Guid = guid;
+
+            return converter.UGuid;
+        }
+
+        public static UGuid NewUGuid()
+        {
+            Converter converter = m_converter;
+
+            converter.Guid = Guid.NewGuid();
+
+            return converter.UGuid;
+        }
+
+        public override string ToString()
+        {
+            Converter converter = m_converter;
+
+            converter.UGuid = this;
+
+            return converter.Guid.ToString("N");
+        }
+    }
+}
